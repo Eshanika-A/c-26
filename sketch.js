52cc66c44c6ce03b012eea3adfdb258492f9b8a9
Eshanika-A <eshanikaappandraj@gmail.com>
2021-01-19 15:36:06 +0530

    we added a new log sprite

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -32,7 +33,9 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
+   
     log5 = new Log(870,120,150, -PI/7);
+    log = new Log(50,100,150, -PI/2);
 
     bird = new Bird(100,100);
 
@@ -58,6 +61,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log.display();
 
     bird.display();
     platform.display();
